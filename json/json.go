c1b4612d2c27747bb62261427ba844c91cd948f5
biezhi <biezhi.me@gmail.com>
2017-04-04 16:08:19 +0800

    :boom: add load json config

--- json/json.go
+++ json/json.go
@@ -1,1 +1,22 @@
 package json
+
+import (
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+)
+
+// load config
+func LoadJson(filename string) (map[string]string, error) {
+	var cmap map[string]string
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("ReadFile: ", err.Error())
+		return nil, err
+	}
+	if err := json.Unmarshal(bytes, &cmap); err != nil {
+		fmt.Println("Unmarshal: ", err.Error())
+		return nil, err
+	}
+	return cmap, nil
+}
