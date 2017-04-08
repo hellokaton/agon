594850d63437f7c652749d08aed40ac89735a1be
biezhi <biezhi.me@gmail.com>
2017-04-09 01:13:32 +0800

    :boom: add PathExist function

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,8 @@
 package utils
 
+import "os"
+
+// Returns a dequanting string array
 func RemoveDuplicatesAndEmpty(a []string) (ret []string){
 	a_len := len(a)
 	for i:=0; i < a_len; i++{
@@ -9,4 +12,13 @@ func RemoveDuplicatesAndEmpty(a []string) (ret []string){
 		ret = append(ret, a[i])
 	}
 	return
+}
+
+// Returns whether the path exists
+func PathExist(_path string) bool {
+	_, err := os.Stat(_path)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
